5661342ec2a2df4cc711576f0a19fcb0493e9cab
zhaojing <1024624013@qq.com>
2018-08-30 00:07:45 +0800

    fix:all images

---
+++ app/src/main/java/com/mdove/easycopy/activity/allimages/adapter/OnPageChangeAdapterListener.java
@@ -0,0 +1,8 @@
+package com.mdove.easycopy.activity.allimages.adapter;
+
+/**
+ * Created by MBENBEN on 2018/8/29.
+ */
+
+public class OnPageChangeAdapterListener {
+}
---
+++ app/src/main/java/com/mdove/easycopy/activity/allimages/model/ShowBitmap.java
@@ -0,0 +1,8 @@
+package com.mdove.easycopy.activity.allimages.model;
+
+/**
+ * Created by MBENBEN on 2018/8/29.
+ */
+
+public class ShowBitmap {
+}
